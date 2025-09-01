687f3a03b29ebc1a068965ff5bc4b2a8a954e25c
adjoly <contact@adjoly.fr>
2025-09-01 17:45:14 +0200

    「🏗️」 wip: added pp

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { request } from 'http';
 const __filename = fileURLToPath(import.meta.url);
 
 const imagesDir = path.join(process.cwd(), 'images');
+const ppDir = path.join(process.cwd(), 'pp');
 const __dirname = path.dirname(__filename)
 
 export default async function(fastify, options) {
@@ -16,8 +17,8 @@ export default async function(fastify, options) {
 		serve: false
 	})
 
-	async function getImageList() {
-		const images = await fs.readdir(imagesDir);
+	async function getImageList(dir) {
+		const images = await fs.readdir(dir);
 
 		return images.map((filename, i) => ({
 			filename,
@@ -26,7 +27,7 @@ export default async function(fastify, options) {
 	}
 
 	fastify.get('/list', async () => {
-		const list = await getImageList();
+		const list = await getImageList(imagesDir);
 		return list.map((filename, i) => ({
 			id: String(i),
 			url: `/id?id=${i}`
@@ -38,7 +39,7 @@ export default async function(fastify, options) {
 		if (typeof id === 'undefined') {
 			return reply.code(400).send({ error: 'Missing id query parameter' })
 		}
-		const list = await getImageList()
+		const list = await getImageList(imagesDir)
 		const image = list.find(img => img.id === id)
 		if (!image) {
 			return reply.code(404).send({ error: 'Image not found' })
@@ -46,8 +47,26 @@ export default async function(fastify, options) {
 		return reply.sendFile(image.filename)
 	});
 
+	fastify.get('/pp', async (request, reply) => {
+		const { id } = request.query
+		const list = await getImageList(ppDir)
+		if (typeof id === 'undefined') {
+			if (list.length === 0) {
+				return reply.code(404).send({ error: 'No images available' })
+			}
+			const randomImage = list[Math.floor(Math.random() * list.length)]
+			return reply.sendFile(randomImage.filename)
+		}
+		const image = list.find(img => img.id === id)
+		if (!image) {
+			return reply.code(404).send({ error: 'Image not found' })
+		}
+		return reply.sendFile(image.filename)
+	});
+
+	
 	fastify.get('/random', async (request, reply) => {
-		const list = await getImageList()
+		const list = await getImageList(imagesDir)
 		if (list.length === 0) {
 			return reply.code(404).send({ error: 'No images available' })
 		}
